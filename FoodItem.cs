71ca6e2d57a9151b71b34ae2c23e931f0a6b2cfa
Vivian Zitek <vivian.zitek@gmail.com>
2022-11-28 23:29:18 -0700

    Add menu and cached item display.

--- FoodItem.cs
+++ FoodItem.cs
@@ -5,13 +5,24 @@ public class FoodItem {
 	public double foodCalories = 0.0;
 	public DateTime timestamp;
 
-	public FoodItem(string name, double calories) {
+	public FoodItem() {
 
-		foodName = name;
-		foodCalories = calories;
 		DateTime currentTime = DateTime.Now;
 		timestamp = currentTime;
 	}
 
+		public static FoodItem CreateItem(FoodItem newItem) {
+
+		Console.Clear();
+
+		// Use Get/UseCursorPosition to have character limit underline
+		Console.Write("Enter food name: ");
+		newItem.foodName = Console.ReadLine();
+
+		Console.Write("Enter food calories: ");
+		newItem.foodCalories = Convert.ToDouble(Console.ReadLine());
+
+		return newItem;
+	}
 
 }
--- Program.cs
+++ Program.cs
@@ -1,22 +1,52 @@
-﻿// Create a program that a user can:
-
-// Enter the name of the food.
-
-// Enter the calories of food.
-
+﻿using menudriver;
 // Saves the time the food was entered and determines if it is most likely breakfast, lunch, or dinner. 
-
-// Saves the date the food was entered.
 
 // Displays the requested data by day.
 
 // Save all of this info and load the info to a file.
 
-FoodItem one = new FoodItem("apple", 3000);
-Console.WriteLine(one.foodName + "\n" + one.foodCalories + "\n" + one.timestamp);
+List<FoodItem> cachedFoodItems = new List<FoodItem>();
 
-bool menuLoop = true;
-while (menuLoop) {
+MenuDriver selectionMenu = new MenuDriver();
+string[] options = new string[] {
 
-	
+	"Add New Food Item",
+	"Load Cached Items",
+};
+selectionMenu.AddOptions(options);
+
+void mainMenu() {
+	while (selectionMenu.menuLoop){
+
+		Console.Clear();
+		selectionMenu.DisplayMenu();
+		selectionMenu.SetMenuCursor();
+
+		switch(selectionMenu.selectedItem) {
+
+			case 0:
+				FoodItem newItem = new FoodItem();
+				cachedFoodItems.Add(FoodItem.CreateItem(newItem));
+				continue;
+			case 1: 
+				displayItems(cachedFoodItems);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+mainMenu();
+
+void displayItems(List<FoodItem> cached) {
+
+	for (int i = 0; i < cached.Count; i++) {
+
+		Console.WriteLine($"[{i+1}] - {cached[i].foodName}");
+		Console.WriteLine($"\tCalories: {cached[i].foodCalories}");
+		Snippet.LineBreak();
+	}
+
+	Console.ReadKey();
 }
